eadd175818120eab736bb9f889162172a3829e89
dagui <davaguir852@gmail.com>
2023-05-01 21:25:59 -0700

    started up Readme file, planned, and psuedocode everthing needed to start

---
+++ index.js
@@ -0,0 +1,22 @@
+const canvas = document.querySelector('canvas')
+const canv = canvas.getContext('2d')
+
+// create character
+
+// move character
+
+//shoot invaders 
+
+// create projectiles
+
+//create invaders 
+
+//grid for invaders 
+
+//allow invaders to shoot back 
+
+// invaders explodig 
+
+// create background 
+
+// score 
